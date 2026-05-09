a9c3b4a9f87feb1d722d9e29dd8b1ee297408a20
Darius Ehsani <dsehsani@ucdavis.edu>
2026-05-09 10:15:02 -0700

    Initial Commit

---
+++ ApolloTests/ApolloTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloTests.swift
+//  ApolloTests
+//
+//  Created by Darius Ehsani on 5/9/26.
+//
+
+import Testing
+@testable import Apollo
+
+struct ApolloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
